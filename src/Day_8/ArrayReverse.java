eea584a2670f661e2e78cb43fdd2a926fc4a3caf
Vighnesh A Sudev <vighneshappu15@gmail.com>
2026-06-29 09:55:26 +0530

    edit

---
+++ src/Day_8/ArrayReverse.java
@@ -0,0 +1,15 @@
+package Day_8;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        String [] arr = {"veg", "non-veg", "sweets","beverages"};
+        String [] m1 = new String[arr.length];
+        for (int i=0;i<arr.length;i++){
+            m1[i] = arr[arr.length-1-i];
+        }
+        System.out.println("Reversed array:");
+        for (int i=0;i<m1.length;i++){
+            System.out.print(m1[i] + " ");
+        }
+    }
+}
