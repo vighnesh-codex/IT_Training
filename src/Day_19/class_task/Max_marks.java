42114c927b4bc841dd6ba98fa998a3b1866f6dc8
Vighnesh A Sudev <vighneshappu15@gmail.com>
2026-07-11 10:17:10 +0530

    edit

---
+++ src/Day_19/class_task/Max_marks.java
@@ -0,0 +1,21 @@
+package Day_19.class_task;
+import java.util.HashMap;
+import java.util.Map; 
+
+public class Max_marks {
+    public static void main(String[] args) {
+        HashMap.<string,Integer> hm = new HashMap<string,Integer>();
+        HashMap.put("Manoj",89);
+        HashMap.put("Vaishnavi",34);
+        int max=0;
+        String name=" ";
+        for(Map.Entry<String,Integer> e:hm.entryset()){
+            if(e.getValue()>max){
+
+            }
+        }
+    }
+
+
+
+}
